b59249d3a393b486a23f16ddc36551cc082609ca
Kritsanaphat <63010040@kmitl.ac.th>
2021-12-02 21:54:03 +0700

    updateQueue

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from tkinter import *
-import tkinter as tk
+import tkinter as tk    
+from collections import deque
+from tkinter import messagebox
 
-class Stack():
+
+
+class Stack:
     def __init__(self,list = None):
         if list == None:
             self.item = []
@@ -25,20 +29,100 @@ class Stack():
             return("Empty")
         return s
 
-class Queue():
-    passlm;ml[]
+class Queue:
+    def __init__(self,list = None):
+        '''
+        if list == None:
+            self.item=[]
+        else:
+            self.item = list
+        '''
+        self.item = deque()
+            
+    def enQueue(self,i):
+        self.item.append(i)
+    
+    def deQueue(self):
+        #return self.item.pop(0)
+        return self.item.popleft()
+    
+    def isEmpty(self):
+        return self.size() == 0
+
+    def size(self):
+        return len(self.item)
+    
+    def __str__(self) :
+        s = self.item[0]
+        i=1
+        if(self.size()>0):
+            while(i<self.size()):
+                s +=","+" "+self.item[i]
+                i+=1
+        return s
+     
 class Sketchpad(Canvas):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
+       
         self.curColor = "black"
-        self.color = {0:"black",1:"red",2:"orange",3:"yellow",4:"green",5:"blue",6:"purple",7:self['background']}
+        self.color = {0:"black",1:"red",2:"orange",3:"yellow",4:"green",5:"blue",6:"purple"}
+        
+        #append fuction changcolor
+        self.queuefuc_color = Queue()
+        self.queuefuc_color.enQueue(self.changeColor0)
+        self.queuefuc_color.enQueue(self.changeColor1)
+        self.queuefuc_color.enQueue(self.changeColor2)
+        self.queuefuc_color.enQueue(self.changeColor3)
+        self.queuefuc_color.enQueue(self.changeColor4)
+        self.queuefuc_color.enQueue(self.changeColor5)
+        self.queuefuc_color.enQueue(self.changeColor6)
+        
+
+        #append color
+        self.queue_color = Queue()
+        for i in range(len(self.color)):
+            self.queue_color.enQueue(self.color[i])
+        print(self.queue_color)
+
+        self.tool()
         self.thickness = 1
         self.buf = []
         self.stack = Stack()
         self.bind("<Button-1>", self.save_posn) 
         self.bind("<B1-Motion>", self.add_line) 
         self.bind("<ButtonRelease-1>", self.save_progress) 
+
+    def tool(self):
+        menubar = Menu(root, background='#ff8000', foreground='black', activebackground='white', activeforeground='black')
+        save_ = Menu(menubar,tearoff=0)
+        menubar.add_cascade(label='Save', menu= save_)
+        save_.add_command(label='Save', command="ff")
+        self.tempfuc_color = []
+        while self.queuefuc_color.isEmpty() is False:
+            self.tempfuc_color.append(self.queuefuc_color.deQueue())
+        print(self.tempfuc_color[0])
+
+
+        self.tempqueue_color = []
+        while self.queue_color.isEmpty() is False:
+            self.tempqueue_color.append(self.queue_color.deQueue())
+        print(self.tempqueue_color[0])
         
+        
+
+        color_ = Menu(menubar,tearoff=0)
+        color_.add_command(background= self.tempqueue_color[0],command=self.tempfuc_color[0])
+        color_.add_command(background= self.tempqueue_color[1],command=self.tempfuc_color[1])
+        color_.add_command(background= self.tempqueue_color[2],command=self.tempfuc_color[2])
+        # color_.add_command(background= self.tempqueue_color[3],command=self.tempfuc_color[3])
+        # color_.add_command(background= self.tempqueue_color[4],command=self.tempfuc_color[4])
+        # color_.add_command(background= self.tempqueue_color[5],command=self.tempfuc_color[5])
+        # color_.add_command(background= self.tempqueue_color[6],command=self.tempfuc_color[6])
+        color_.add_command(label="other",command=self.other)
+        menubar.add_cascade(label='Color',menu = color_)
+        root.config(menu = menubar)
+
     def save_posn(self, event):
         self.lastx, self.lasty = event.x, event.y
 
@@ -57,7 +141,7 @@ class Sketchpad(Canvas):
         if len(self.buf) != 0:
             self.stack.push(self.buf.copy())
             self.buf.clear()
-            print(self.stack)
+            # print(self.stack)
 
     def openFile(self):
         pass
@@ -66,56 +150,132 @@ class Sketchpad(Canvas):
         pass
 
     def changeColor0(self):
-        self.curColor = self.color[0]
+        colorChange = "black"
+        if self.tempqueue_color[0] != colorChange:
+            self.curColor =colorChange
+            self.queue_color.enQueue(colorChange)
+            self.queuefuc_color.enQueue(self.tempfuc_color[0])
+            for i in range(len(self.tempqueue_color)):
+                if self.tempqueue_color[i]!=colorChange:
+                    self.queue_color.enQueue(self.tempqueue_color[i])
+                    self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        else:
+            for i in range(len(self.tempqueue_color)):
+                self.queue_color.enQueue(self.tempqueue_color[i])
+                self.queuefuc_color.enQueue(self.tempfuc_color[i])
+
+        self.tool()
 
     def changeColor1(self):
-        self.curColor = self.color[1]
+        colorChange = "red"
+        if self.tempqueue_color[0] != colorChange:
+            self.curColor =colorChange
+            self.queue_color.enQueue(colorChange)
+            self.queuefuc_color.enQueue(self.tempfuc_color[1])
+            for i in range(len(self.tempqueue_color)):
+                if self.tempqueue_color[i]!=colorChange:
+                    self.queue_color.enQueue(self.tempqueue_color[i])
+                    self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        else:
+            for i in range(len(self.tempqueue_color)):
+                self.queue_color.enQueue(self.tempqueue_color[i])
+                self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        self.tool()
 
     def changeColor2(self):
-        self.curColor = self.color[2]
-
-    def changeColor3(self):
-        self.curColor = self.color[3]
-
-    def changeColor4(self):
-        self.curColor = self.color[4]
-
-    def changeColor5(self):
-        self.curColor = self.color[5]
-
-    def changeColor6(self):
-        self.curColor = self.color[6]
-
-    def changeColor7(self):
-        self.curColor = self.color[7]
-    
-    def ChangeThickness0(self):
-        self.thicknes = 1
-    
-    def ChangeThickness1(self):
-        self.thicknes = 3
-
-    def ChangeThickness2(self):
-        self.thicknes = 5
+        colorChange = "orange"
+        if self.tempqueue_color[0] != colorChange:
+            self.curColor =colorChange
+            self.queue_color.enQueue(colorChange)
+            self.queuefuc_color.enQueue(self.tempfuc_color[2])
+            for i in range(len(self.tempqueue_color)):
+                if self.tempqueue_color[i]!=colorChange:
+                    self.queue_color.enQueue(self.tempqueue_color[i])
+                    self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        else:
+            for i in range(len(self.tempqueue_color)):
+                self.queue_color.enQueue(self.tempqueue_color[i])
+                self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        self.tool()
+        
         
 
+    def changeColor3(self):
+        colorChange = "yellow"
+        if self.tempqueue_color[0] != colorChange:
+            self.curColor =colorChange
+            self.queue_color.enQueue(colorChange)
+            self.queuefuc_color.enQueue(self.tempfuc_color[3])
+            for i in range(len(self.tempqueue_color)):
+                if self.tempqueue_color[i]!=colorChange:
+                    self.queue_color.enQueue(self.tempqueue_color[i])
+                    self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        else:
+            for i in range(len(self.tempqueue_color)):
+                self.queue_color.enQueue(self.tempqueue_color[i])
+                self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        self.tool()
+
+    def changeColor4(self):
+        colorChange = "green"
+        if self.tempqueue_color[0] != colorChange:
+            self.curColor =colorChange
+            self.queue_color.enQueue(colorChange)
+            self.queuefuc_color.enQueue(self.tempfuc_color[4])
+            for i in range(len(self.tempqueue_color)):
+                if self.tempqueue_color[i]!=colorChange:
+                    self.queue_color.enQueue(self.tempqueue_color[i])
+                    self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        else:
+            for i in range(len(self.tempqueue_color)):
+                self.queue_color.enQueue(self.tempqueue_color[i])
+                self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        self.tool()
+        
+    def changeColor5(self):
+        colorChange = "blue"
+        if self.tempqueue_color[0] != colorChange:
+            self.curColor =colorChange
+            self.queue_color.enQueue(colorChange)
+            self.queuefuc_color.enQueue(self.tempfuc_color[5])
+            for i in range(len(self.tempqueue_color)):
+                if self.tempqueue_color[i]!=colorChange:
+                    self.queue_color.enQueue(self.tempqueue_color[i])
+                    self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        else:
+            for i in range(len(self.tempqueue_color)):
+                self.queue_color.enQueue(self.tempqueue_color[i])
+                self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        self.tool()
+        
+
+    def changeColor6(self):
+        colorChange = "purple"
+        if self.tempqueue_color[0] != colorChange:
+            self.curColor =colorChange
+            self.queue_color.enQueue(colorChange)
+            self.queuefuc_color.enQueue(self.tempfuc_color[6])
+            for i in range(len(self.tempqueue_color)):
+                if self.tempqueue_color[i]!=colorChange:
+                    self.queue_color.enQueue(self.tempqueue_color[i])
+                    self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        else:
+            for i in range(len(self.tempqueue_color)):
+                self.queue_color.enQueue(self.tempqueue_color[i])
+                self.queuefuc_color.enQueue(self.tempfuc_color[i])
+        self.tool()
+    
+    def other(self):
+        pass
+
+
 root = Tk()
-menubar = Menu(root)
-
-save_ = Menu(menubar,tearoff=0)
-menubar.add_cascade(label='Save', menu= save_)
-save_.add_command(label='Save', command=None)
-
-
-color = Menu(menubar,tearoff=0)
-menubar.add_cascade(label='Color',menu = color)
-color.add_command(background='red',command=None)
+sketch = Sketchpad(root)
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(1, weight=1)
-sketch = Sketchpad(root)
 sketch.bind()
 sketch.grid(column=0, row=1, sticky=(N, W, E, S))
 
-root.config(menu = menubar)
-mainloop()
+
+root.mainloop()
--- test.py
+++ test.py
@@ -1,45 +1,53 @@
+from tkinter import *
+from tkinter import messagebox
 
-# importing only  those functions 
-# which are needed
-from tkinter import * 
-from tkinter.ttk import * 
-from time import strftime
-  
-# creating tkinter window
-root = Tk()
-root.title('Menu Demonstration')
-  
-# Creating Menubar
-menubar = Menu(root)
-  
-# Adding File Menu and commands
-file = Menu(menubar, tearoff = 0)
-menubar.add_cascade(label ='File', menu = file)
-file.add_command(label ='New File', command = None)
-file.add_command(label ='Open...', command = None)
-file.add_command(label ='Save', command = None)
-file.add_separator()
-file.add_command(label ='Exit', command = root.destroy)
-  
-# Adding Edit Menu and commands
-edit = Menu(menubar, tearoff = 0)
-menubar.add_cascade(label ='Edit', menu = edit)
-edit.add_command(label ='Cut', command = None)
-edit.add_command(label ='Copy', command = None)
-edit.add_command(label ='Paste', command = None)
-edit.add_command(label ='Select All', command = None)
-edit.add_separator()
-edit.add_command(label ='Find...', command = None)
-edit.add_command(label ='Find again', command = None)
-  
-# Adding Help Menu
-help_ = Menu(menubar, tearoff = 0)
-menubar.add_cascade(label ='Help', menu = help_)
-help_.add_command(label ='Tk Help', command = None)
-help_.add_command(label ='Demo', command = None)
-help_.add_separator()
-help_.add_command(label ='About Tk', command = None)
-  
-# display Menu
-root.config(menu = menubar)
-mainloop()
+class MenuBar(Menu):
+    def __init__(self, ws):
+        Menu.__init__(self, ws)
+
+        file = Menu(self, tearoff=False)
+        file.add_command(label="New")  
+        file.add_command(label="Open")  
+        file.add_command(label="Save")  
+        file.add_command(label="Save as")    
+        file.add_separator()
+        file.add_command(label="Exit", underline=1, command=self.quit)
+        self.add_cascade(label="File",underline=0, menu=file)
+        
+        edit = Menu(self, tearoff=0)  
+        edit.add_command(label="Undo")  
+        edit.add_separator()     
+        edit.add_command(label="Cut")  
+        edit.add_command(label="Copy")  
+        edit.add_command(label="Paste")  
+        self.add_cascade(label="Edit", menu=edit) 
+
+        view = Menu(self, tearoff=0)
+        ratio = Menu(self, tearoff=0)
+        for aspected_ratio in ('4:3', '16:9'):
+            ratio.add_command(label=aspected_ratio)
+        view.add_cascade(label='Ratio', menu=ratio)
+        self.add_cascade(label='View', menu=view)
+
+        help = Menu(self, tearoff=0)  
+        help.add_command(label="About", command=self.about)  
+        self.add_cascade(label="Help", menu=help)  
+
+    def exit(self):
+        self.exit
+
+    def about(self):
+            messagebox.showinfo('PythonGuides', 'Python Guides aims at providing best practical tutorials')
+
+
+class MenuDemo(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        menubar = MenuBar(self)
+        self.config(menu=menubar)
+
+if __name__ == "__main__":
+    ws=MenuDemo()
+    ws.title('Python Guides')
+    ws.geometry('300x200')
+    ws.mainloop()
