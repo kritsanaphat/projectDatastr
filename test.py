74cd4713f310a97eb3099da352340295ef169f93
Kritsanaphat <63010040@kmitl.ac.th>
2021-12-21 11:12:47 +0700

    finaly

--- test.py
+++ test.py
@@ -1,66 +1,26 @@
-from tkinter import Tk, Menu
-
-
-# root window
+# importing all files  from tkinter
+from tkinter import * 
+from tkinter import ttk
+  
+# import only asksaveasfile from filedialog
+# which is used to save file in any extension
+from tkinter.filedialog import asksaveasfile
+  
 root = Tk()
-root.geometry('320x150')
-root.title('Menu Demo')
-
-
-# create a menubar
-menubar = Menu(root)
-root.config(menu=menubar)
-
-# create the file_menu
-file_menu = Menu(
-    menubar,
-    tearoff=0
-)
-
-# add menu items to the File menu
-file_menu.add_command(label='New')
-file_menu.add_command(label='Open...')
-file_menu.add_command(label='Close')
-file_menu.add_separator()
-
-# add a submenu
-sub_menu = Menu(file_menu, tearoff=0)
-sub_menu.add_command(label='Keyboard Shortcuts')
-sub_menu.add_command(label='Color Themes')
-
-# add the File menu to the menubar
-file_menu.add_cascade(
-    label="Preferences",
-    menu=sub_menu
-)
-
-# add Exit menu item
-file_menu.add_separator()
-file_menu.add_command(
-    label='Exit',
-    command=root.destroy
-)
-
-
-menubar.add_cascade(
-    label="File",
-    menu=file_menu,
-    underline=0
-)
-# create the Help menu
-help_menu = Menu(
-    menubar,
-    tearoff=0
-)
-
-help_menu.add_command(label='Welcome')
-help_menu.add_command(label='About...')
-
-# add the Help menu to the menubar
-menubar.add_cascade(
-    label="Help",
-    menu=help_menu,
-    underline=0
-)
-
-root.mainloop()
+root.geometry('200x150')
+  
+# function to call when user press
+# the save button, a filedialog will
+# open and ask to save file
+def save():
+    files = [('All Files', '*.*'), 
+             ('Python Files', '*.py'),
+             ('Text Document', '*.txt')
+             ]
+    file = asksaveasfile(filetypes = files, defaultextension = files)
+  
+btn = ttk.Button(root, text = 'Save', command = lambda : save())
+btn.pack(side = TOP, pady = 20)
+  
+mainloop()
+mainloop()
